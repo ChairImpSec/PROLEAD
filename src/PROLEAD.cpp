b3cd4443ba8b6d7f4472b4e6255e2a153355b016
Nicolai Müller <nicolai.mueller@ruhr-uni-bochum.de>
2022-09-01 19:53:37 +0200

    Check command line parameters

--- src/PROLEAD.cpp
+++ src/PROLEAD.cpp
@@ -1,8 +1,8 @@
-
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cinttypes>
+#include <filesystem> 
+#include <iostream>
+#include <string>
+#include <vector>
 
 #include "Util/CommandLineParameter.hpp"
 #include "Hardware/Execute.hpp"
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
     CommandLineParameterStruct CommandLineParameters;
     uint8_t i = 1, res = 0;
     bool Hardware = true;
+    std::error_code ec;
 
     if (argc > 1){
  		if ((CommandLineArguments.at(1) ==  "-h") || (CommandLineArguments.at(1) == "-?") || (CommandLineArguments.at(1) == "/?") || (CommandLineArguments.at(1) == "-help")){
@@ -25,6 +26,11 @@ int main(int argc, char *argv[])
     {
         if ((CommandLineArguments.at(i) == "-lf") || (CommandLineArguments.at(i) == "-libraryfile")){
             CommandLineParameters.LibraryFileName = CommandLineArguments.at(i + 1);
+            std::filesystem::path path(CommandLineParameters.LibraryFileName);
+
+            if (std::filesystem::is_directory(path, ec)){
+                throw std::runtime_error("Library file path points to a directory!");
+            }
             i += 2;
         }
 		else
@@ -35,6 +41,11 @@ int main(int argc, char *argv[])
         else
         if ((CommandLineArguments.at(i) == "-df") || (CommandLineArguments.at(i) == "-designfile")){
             CommandLineParameters.DesignFileName = CommandLineArguments.at(i + 1);
+            std::filesystem::path path(CommandLineParameters.DesignFileName);
+
+            if (std::filesystem::is_directory(path, ec)){
+                throw std::runtime_error("Design file path points to a directory!");
+            }
             i += 2;
         }
         else
@@ -45,11 +56,21 @@ int main(int argc, char *argv[])
 		else
         if ((CommandLineArguments.at(i) == "-cf") || (CommandLineArguments.at(i) == "-configfile")){
             CommandLineParameters.SettingsFileName = CommandLineArguments.at(i + 1);
+            std::filesystem::path path(CommandLineParameters.SettingsFileName);
+
+            if (std::filesystem::is_directory(path, ec)){
+                throw std::runtime_error("Config file path points to a directory!");
+            }
             i += 2;
         } 
 		else
         if ((CommandLineArguments.at(i) == "-rf") || (CommandLineArguments.at(i) == "-resultfolder")){
             CommandLineParameters.EvaluationResultFolderName = CommandLineArguments.at(i + 1);
+            std::filesystem::path path(CommandLineParameters.EvaluationResultFolderName);
+
+            if (std::filesystem::is_regular_file(path, ec)){
+                throw std::runtime_error("Result folder path points to a file!");
+            }
             i += 2;
         }
         else    
@@ -59,7 +80,12 @@ int main(int argc, char *argv[])
         }  
         else    
         if ((CommandLineArguments.at(i) == "-ldf") || (CommandLineArguments.at(i) == "-linkerfile")){
-            CommandLineParameters.LinkerFileName = CommandLineArguments.at(i + 1);            
+            CommandLineParameters.LinkerFileName = CommandLineArguments.at(i + 1);
+            std::filesystem::path path(CommandLineParameters.LinkerFileName);  
+
+            if (std::filesystem::is_directory(path, ec)){
+                throw std::runtime_error("Linker file path points to a directory!");
+            }          
             i += 2;
         }  
         else
@@ -77,7 +103,7 @@ int main(int argc, char *argv[])
         printf("Error: Platform is not specified correctly!");
         res = 1;
     }
-    
+
     if (!res){
         if (Hardware){
             Hardware::Execute(CommandLineParameters);
