2111177c20de2b8953e3e9f4bf9f9d7f90f54824
Nicolai Müller <nicolai.mueller@ruhr-uni-bochum.de>
2022-11-29 11:15:36 +0100

    improved execution scripts

--- examples/PRESENT_NullFresh_d1/results/compact_mode/run.sh
+++ examples/PRESENT_NullFresh_d1/results/compact_mode/run.sh
@@ -1,1 +1,1 @@
-./../../../../release/PROLEAD -lf ../../../../library.lib -df ../../gate/design.v -cf config.set -rf ../compact_mode > Report.dat
+./../../../../release/PROLEAD -lf ../../../../library.lib -df ../../gate/design.v -cf config.set 2>&1 | tee Report.dat
