055edafdf57d75f4ea529914d3f2c3d1fc4cafeb
Nicolai Müller <nicolai.mueller@ruhr-uni-bochum.de>
2023-02-08 09:22:05 +0100

    update documentation

--- docs/search/typedefs_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['u16_0',['u16',['../namespacemulator.html#ae1a1d87c2c063291a14284ebbaddc812',1,'mulator']]],
-  ['u32_1',['u32',['../namespacemulator.html#a2767fcbf22c6cd29a003ed5343850adc',1,'mulator']]],
-  ['u64_2',['u64',['../namespacemulator.html#a1a4eb2c0de0eebcf6b15e4dedd5b654f',1,'mulator']]],
-  ['u8_3',['u8',['../namespacemulator.html#abf344b95a6f3b3627027bc60418892d2',1,'mulator']]]
-];
