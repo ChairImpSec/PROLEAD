9cd21abcf59369660ae62b0aca4773274394ef62
Nicolai Müller <nicolai.mueller@ruhr-uni-bochum.de>
2023-12-29 18:37:17 +0100

    Update comments

--- inc/Util/CpuCoreSelector.hpp
+++ inc/Util/CpuCoreSelector.hpp
@@ -2,23 +2,21 @@
 #pragma once
 #include <thread>
 
-enum class CpuSelectionOption {
-    all, half, third, quarter, specific
+enum class CpuSelectionOption { all, half, third, quarter, specific };
+
+/**
+ * The class encapsulates the logic for determining the number of threads,
+ * making it reusable and easy to integrate.
+ */
+class CpuCoreSelector {
+ public:
+  /**
+   * @brief Retrieves the calculated number of threads.
+   * @param option The specified cpu option.
+   * @param specified_number_of_cores The specified number of cores to use.
+   * @return The number of threads.
+   * @author https://github.com/gmottajr
+   */
+  static unsigned int getOptimalCount(
+      CpuSelectionOption option, unsigned int specified_number_of_cores = 0);
 };
-
-
-/// <summary>
-///     The CpuCoreSelector class takes a std::string in its constructor, which represents the setting for max_no_of_threads.
-///     It calculates the optimal number of threads in the updateThreadCount method based on this setting.
-///     getOptimalThreadCount method can be used to retrieve the calculated number of threads.
-///     The class encapsulates the logic for determining the number of threads, making it reusable and easy to integrate.
-/// </summary>
-class CpuCoreSelector
-{
-public:
-    static unsigned int getOptimalCount(CpuSelectionOption option, unsigned int specified_number_of_cores = 0);
-};
-
-
-
-
