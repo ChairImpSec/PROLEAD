5414d89decfd94f638f4461650a06bdb3925adf6
Felix Uhle <felix.uhle@rub.de>
2024-09-20 12:32:37 +0200

    bump version

--- prolead.nix
+++ prolead.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation {
   pname = "prolead";
-  version = "v3.0.2-alpha";
+  version = "v3.0.3-alpha";
 
   src = fetchFromGitHub {
     owner = "ChairImpSec";
     repo = "PROLEAD";
-    rev = "v3.0.2-alpha";
-    sha256 = "WZ0Quxa2R1m4490XzibXUFV6dYo4BWiHvcHR8panUj8=";
+    rev = "v3.0.3-alpha";
+    sha256 = "2nfUc9q/DnCsme+90wtzHWRFHsSOXQ0OHj/AqhgoerI=";
   };
 
   # Required for building
