f70fc753052f4a5c63a16011941561523a39568f
Felix Uhle <felix.uhle@rub.de>
2025-02-17 11:14:02 +0100

    update to newest nix package channel

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 let
 
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
 
   pkgs = import nixpkgs { config = {}; overlays = []; };
 
